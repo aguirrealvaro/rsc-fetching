18d80df77fda4702ce2c620df0df68a87e64eae4
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-08 20:08:39 -0300

    add cn to skeleton

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,8 +1,12 @@
+import { cn } from "@/utils/cn";
+
 export const Skeleton = () => {
   return (
     <div className="grid grid-cols-4 gap-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
       {Array.from(Array(16).keys()).map((key) => {
-        return <div key={key} className="h-48 animate-pulse rounded-md bg-gray-200" />;
+        return (
+          <div key={key} className={cn("animate-pulse rounded-md bg-gray-200", "h-48")} />
+        );
       })}
     </div>
   );
